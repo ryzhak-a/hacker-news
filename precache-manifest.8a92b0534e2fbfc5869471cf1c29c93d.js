a301e70f9ec8b410aa858a024bf3486192b1669b
Andrii Ryzhak <55483532+ryzhak-andrii@users.noreply.github.com>
2020-09-29 12:34:29 +0300

    Updates

---
+++ precache-manifest.8a92b0534e2fbfc5869471cf1c29c93d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d972dcc09252c15d2318a6b1f2da4948",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "70e1edbdc97e535260e3",
+    "url": "/hacker-news/static/css/main.bd1c84fa.chunk.css"
+  },
+  {
+    "revision": "a373e8dea1e25a220f0f",
+    "url": "/hacker-news/static/js/2.b92dc6fd.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/hacker-news/static/js/2.b92dc6fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70e1edbdc97e535260e3",
+    "url": "/hacker-news/static/js/main.dd548989.chunk.js"
+  },
+  {
+    "revision": "f934003405a453338133",
+    "url": "/hacker-news/static/js/runtime-main.bdc9e62f.js"
+  }
+]);
